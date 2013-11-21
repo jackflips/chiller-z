11e7b2cde701b33ae6b08ed5cfd081b7d6501333
John Rogers <johnrogers@Johns-MacBook-Pro.local>
2013-11-20 19:59:02 -0800

    rewrote the engine

--- game.js
+++ game.js
@@ -1,48 +1,32 @@
-function clone(obj) {
-    if (null == obj || "object" != typeof obj) return obj;
-    if (obj instanceof Date) {
-        var copy = new Date();
-        copy.setTime(obj.getTime());
-        return copy;
-    }
-    if (obj instanceof Array) {
-        var copy = [];
-        for (var i = 0, len = obj.length; i < len; i++) {
-            copy[i] = clone(obj[i]);
-        }
-        return copy;
-    }
-    if (obj instanceof Object) {
-        var copy = {};
-        for (var attr in obj) {
-            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
-        }
-        return copy;
-    }
-    throw new Error("Unable to copy obj! Its type isn't supported.");
-}
 
 var game;
-var MAP_WIDTH = 21;
-var MAP_HEIGHT = 11;
-textures = [];
+var sprites = [];
+var debugCounter = 0;
+
+var MOVEMENT_RATE = 10;
 
 function Point(x, y) {
 	this.x = x;
 	this.y = y;
 
-	this.addPoints = function(point) {
+	this.addPoint = function(point) {
 		newPoint = new Point();
 		newPoint.x = this.x + point.x;
 		newPoint.y = this.y + point.y;
 		return newPoint;
+	}
+
+	this.subtractPoint = function(point) {
+		return this.addPoint(new Point(-point.x, -point.y));
 	}
 }
 
 function Game() {
 	this.map;
 	this.center;
-	this.currentContext;
+	this.canvas;
+	this.context;
+	this.currentMove;
 }
 
 function isoTo2D(point) {
@@ -68,37 +52,12 @@ window.requestAnimFrame = (function(){
           };
 })();
 
-
-function loadTextures(next) {
-	textures = [];
-	var numLoaded = 0;
-	function imageLoaded() { //this looks hacky because the calls are asynchronus. not my fault-ish -jack
-		if (numLoaded >= 1) {
-			next(textures);
-		}
-		numLoaded++;
-	}
-	var grassImage = new Image();
-	grassImage.src = "grass.png";
-	textures[0] = grassImage;
-	var gravelImage = new Image()
-	gravelImage.src = "gravel.jpg";
-	grassImage.onload = function() {
-		textures[0] = grassImage;
-		imageLoaded();
-	}
-	gravelImage.onload = function() {
-		textures[1] = gravelImage;
-		imageLoaded();
-	}
-}
-
 function generateMap() {
 	map = [];
 	for (i=-200; i<200; i++) {
 		map[i] = []
 		for (j=-200; j<200; j++) {
-			if (Math.random() < 0.9) 
+			if (Math.random() < 0.7) 
 	    		map[i][j] = 0;
 	    	else 
 	    		map[i][j] = 1;
@@ -109,113 +68,100 @@ function generateMap() {
 
 function animate() {
     requestAnimFrame( animate );
+    game.context.clearRect(0, 0, game.canvas.width, game.canvas.height);
     draw();
+}
+
+function Sprite(tileType, pos, size) {
+	if (debugCounter % 100 == 0) {
+	}
+	if (tileType == 0) {
+		this.tileType = "grass.png";
+	}
+	else {
+		this.tileType = "dirt.png";
+	}
+    this.pos = pos;
+    this.size = size;
+}
+
+Sprite.prototype.staticRender = function(ctx) {
+    var x = this.pos[0];
+    var y = this.pos[1];
+
+    ctx.drawImage(resources.get(this.tileType),
+                  0, 0,
+                  this.size[0], this.size[1],
+                  x, y,
+                  this.size[0], this.size[1]);
 }
 
 
 function draw() {
 
-    var squareSideLength = game.currentContext.canvas.width / 20;
+	var firstRowToDraw = Math.floor(game.center.x / 96);
+	var firstColToDraw = Math.floor(game.center.y / 96);
 
-    centerInTiles = clone(game.center);
-    centerInTiles.x = Math.floor(centerInTiles.x / squareSideLength);
-    centerInTiles.y = Math.floor(centerInTiles.y / squareSideLength);
-
-	for (i=0 - centerInTiles.x - 1; i<MAP_WIDTH - centerInTiles.x - 1; i++) {
-		for (j=-MAP_HEIGHT - (centerInTiles.y + 1); j<MAP_HEIGHT - (centerInTiles.y + 1); j++) {
-	    	game.currentContext.beginPath();
-	    	rectPoint = new Point((i * squareSideLength) + game.center.x, (j * squareSideLength) + game.center.y);
-	    	points = new Array(4);
-	    	points[0] = new Point(rectPoint.x, rectPoint.y);
-	    	points[1] = new Point(rectPoint.x, rectPoint.y+squareSideLength);
-	    	points[2] = new Point(rectPoint.x + squareSideLength, rectPoint.y + squareSideLength);
-	    	points[3] = new Point(rectPoint.x + squareSideLength, rectPoint.y);
-	    	for (k=0; k<4; k++) {
-	    		points[k] = twoDToIso(points[k]);
-	    	}
-
-			game.currentContext.moveTo(points[0].x, points[0].y); //constants chosen experimentally
-			game.currentContext.lineTo(points[1].x, points[1].y);
-			game.currentContext.lineTo(points[2].x, points[2].y);
-			game.currentContext.lineTo(points[3].x, points[3].y);
-	    	
-
-
-			//drawing code
-	    	var grassPattern = game.currentContext.createPattern(textures[0], 'repeat');
-    		var gravelPattern = game.currentContext.createPattern(textures[1], 'repeat');
-	    	if (game.map[i][j] == 0) 
-	    		game.currentContext.fillStyle = grassPattern;
-	    	else
-	    		game.currentContext.fillStyle = gravelPattern;
-	    	game.currentContext.lineWidth = 1.5;
-	    	game.currentContext.strokeStyle = 'black';
-	    	game.currentContext.fill();
-	    	game.currentContext.stroke();
-    	}
-    } 
-
-    /*
-    //for (i=centerInTiles.x; i<MAP_WIDTH + centerInTiles.x; i++) {
-		//for (j=-MAP_HEIGHT + centerInTiles.y; j<MAP_HEIGHT+centerInTiles.y; j++) {
-			//debug information
-			ctx.beginPath();
-			rectPoint = new Point((i + centerInTiles) * squareSideLength, (j + centerInTiles) * squareSideLength);
-	    	points = new Array(4);
-	    	points[0] = new Point(rectPoint.x, rectPoint.y);
-	    	points[1] = new Point(rectPoint.x, rectPoint.y+squareSideLength);
-	    	points[2] = new Point(rectPoint.x + squareSideLength, rectPoint.y + squareSideLength);
-	    	points[3] = new Point(rectPoint.x + squareSideLength, rectPoint.y);
-	    	for (k=0; k<4; k++) {
-	    		points[k] = twoDToIso(points[k]);
-	    	}
-			ctx.fillStyle = 'black';
-	        ctx.font="16px Arial";
-	    	ctx.fillText(21*j + i + " ", points[0].x + 2, points[0].y + 70);
-	    	//end debug info
+	for (i=firstRowToDraw; i<firstRowToDraw + Math.ceil(game.canvas.width / 96) + 1; i++) {
+		for (j=firstColToDraw; j<firstColToDraw + Math.ceil(game.canvas.height / 96) + 1; j++) {
+			sprites.push(new Sprite(game.map[i][j], [i*96 - game.center.x, j*96 - game.center.y], [96, 96]));
 		}
 	}
-	*/
+
+	for (sprite in sprites) {
+		sprites[sprite].staticRender(game.context);
+	}
+
+	debugCounter++;
 }
 
+function bindKeys() {
+	$(document).keydown(function(e){
+		e.preventDefault();
+    	if (e.keyCode == 37) { 
+    		game.center.x = game.center.x - MOVEMENT_RATE;
+    	}
+    	if (e.keyCode == 38) {
+    		game.center.y = game.center.y - MOVEMENT_RATE;
+    	}
+    	if (e.keyCode == 39) {
+    		game.center.x = game.center.x + MOVEMENT_RATE;
+    	}
+    	if (e.keyCode == 40) {	
+    		game.center.y = game.center.y + MOVEMENT_RATE;
+    	}
+	});
+
+	$('#canvas').on('contextmenu',function() {
+		return false; //disables the right click menu on the canvas
+	});
+
+	$('#canvas').mousedown(function(e) {
+		e.preventDefault();
+		if (e.which == 3) {
+			var offset = $(this).offset();
+    		var mouseOffset = new Point(e.clientX - offset.left, e.clientY - offset.top);
+    		var gamespaceOffset = mouseOffset.subtractPoint(new Point(canvas.width/2, canvas.height/2));
+
+		}
+	});
+}
 
 $(function() { //jquery loaded
 	game = new Game();
 	game.center = new Point(0, 0);
 	game.map = generateMap();
-	var ctx = $("#canvas")[0].getContext('2d');
-	var ctx2 = $("#canvas2")[0].getContext('2d');
-	game.currentContext = ctx;
-	game.otherContext = ctx2;
-	game.currentContext.canvas.width  = window.innerWidth;
-    game.currentContext.canvas.height = window.innerHeight;
-    game.otherContext.canvas.width  = window.innerWidth;
-    game.otherContext.canvas.height = window.innerHeight;
-	loadTextures(function(textures) { //load textures, then:
-		animate()
-	});
-
-	$(document).keydown(function(e){
-		var ctx = $("#canvas")[0].getContext('2d');
-		ctx.canvas.width  = window.innerWidth;
-    	ctx.canvas.height = window.innerHeight;
-    	var squareSideLength = ctx.canvas.width / 20;
-    	if (e.keyCode == 37) { 
-    		game.center = game.center.addPoints(isoTo2D(new Point(-10, 0)));
-    		return false;
-    	}
-    	if (e.keyCode == 38) {
-    		game.center = game.center.addPoints(isoTo2D(new Point(0, -10)));
-    		return false;
-    	}
-    	if (e.keyCode == 39) {
-    		game.center = game.center.addPoints(isoTo2D(new Point(10, 0)));
-    		return false;
-    	}
-    	if (e.keyCode == 40) {	
-    		game.center = game.center.addPoints(isoTo2D(new Point(0, 10)));
-    		return false;
-    	}
-	});
-
+	game.canvas = $("#canvas")[0];
+	game.canvas.width  = 500;
+    game.canvas.height = 500;
+	game.context = game.canvas.getContext('2d');
+    game.screenTileWidth = Math.floor(window.innerWidth / 96) + 1;
+    game.screenTileHeight = Math.floor(window.innerHeight / 96) + 1;
+    resources.load([
+    	'grass.png',
+    	'dirt.png',
+    	'car.png'
+   	]);
+    resources.onReady(animate);
+    bindKeys();
 });
