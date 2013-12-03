70d297cc28c3809f68adb1c441b3b9cb87109db9
artiichoke <artiichoke@gmail.com>
2013-12-02 23:25:55 -0800

    little update

--- human.js
+++ human.js
@@ -5,16 +5,36 @@ function Human (position, velocity) {
 }
 
 // Conditions
+var closeToZombie = new Condition();
+closeToZombie.test = function() {
+}
+var caughtByZombie = new Condition();
+caughtByZombie.test = function() {
+}
+var timeLapse = new Condition();
+timeLapse.test = function() {
+}
 
 // Actions
+var wanderAction = function() {}
+var 
+
 
 // Transition Lists
 
 // States
+var wander = new State();
+var run = new State();
+var beingEaten = new State();
 
 // Transitions
+var zombieClose = new Transition();
+zombieClose.setTargetState(run);
+zombieClose.setCondition(closeToZombie);
 
 // State Machine
+var humanFSM = new StateMachine();
+humanFSM.setCurrentState(wander);
 
 Human.prototype.update = function(game){
 
